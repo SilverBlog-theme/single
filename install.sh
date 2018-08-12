c61b0da3729c4766a1e35008ecbbeffe48d44e97
tcdw <tcdw2011@gmail.com>
2018-08-13 00:12:26 +0800

    添加安装脚本

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -o errexit
+templates_name="single"
+
+if [ $(basename `pwd`) != "templates" ];then
+    echo "[Error] Please do this in the templates directory!"
+    exit
+fi
+if [ ! -d ${templates_name} ]; then
+    git clone https://github.com/SilverBlogTheme/${templates_name}.git
+fi
+ln -sv ../${templates_name}/static ./static/${templates_name}
+cd ${templates_name}
+if [ -f "config.example.json" ]; then
+    cp config.example.json config.json
+fi
